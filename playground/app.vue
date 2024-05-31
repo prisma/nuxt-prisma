016eee9808b11b9d6313109477db3cc57813be6d
Ankur Datta <datta@prisma.io>
2024-05-31 14:25:30 +0600

    fix: resolve 500 error

--- playground/app.vue
+++ playground/app.vue
@@ -1,14 +1,18 @@
+<script lang="ts" setup>
+  async function main() {
+    try {
+    const prisma = usePrismaClient()
+    const posts = await prisma.post.findMany()
+    console.log(posts)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  main()
+</script>
+
 <template>
   <div>
     Nuxt module playground!
   </div>
 </template>
-
-<script lang="ts" setup>
-  async function main() {
-    const prisma = usePrismaClient()
-    const posts = await prisma.post.findMany()
-    console.log(posts)
-  }
-  main()
-</script>
--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,5 +1,6 @@
 export default defineNuxtConfig({
   modules: ['../src/module'],
   prisma: {},
+  ssr: true,
   devtools: { enabled: true }
 })

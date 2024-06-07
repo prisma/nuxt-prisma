46f682b360ced33bd791137effb447cb6592e0f7
Ankur Datta <datta@prisma.io>
2024-06-07 19:35:48 +0600

    fix: add og image path

--- docs/app.config.ts
+++ docs/app.config.ts
@@ -1,32 +1,32 @@
 // https://github.com/nuxt-themes/docus/blob/main/nuxt.schema.ts
 export default defineAppConfig({
   docus: {
-    title: 'Nuxt Prisma',
-    description: 'Add Prisma ORM easily to your Nuxt apps.',
-    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    title: "Nuxt Prisma",
+    description: "Add Prisma ORM easily to your Nuxt apps.",
+    image: "/cover.png",
     socials: {
-      twitter: 'prisma',
-      github: 'prisma/nuxt-prisma',
+      twitter: "prisma",
+      github: "prisma/nuxt-prisma",
       nuxt: {
-        label: 'Nuxt',
-        icon: 'simple-icons:nuxtdotjs',
-        href: 'https://nuxt.com'
-      }
+        label: "Nuxt",
+        icon: "simple-icons:nuxtdotjs",
+        href: "https://nuxt.com",
+      },
     },
     aside: {
       level: 0,
       collapsed: false,
-      exclude: []
+      exclude: [],
     },
     main: {
       padded: true,
-      fluid: true
+      fluid: true,
     },
     header: {
       logo: true,
       showLinkIcon: true,
       exclude: [],
-      fluid: true, 
-    }
-  }
-})
+      fluid: true,
+    },
+  },
+});
